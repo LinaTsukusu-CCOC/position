af5807373f2a3d270d589fd9aa21b3fdd91f6b61
neverclear <neverclear86sab@gmail.com>
2020-07-07 00:39:18 +0900

    ok

---
+++ position.lua
@@ -0,0 +1,31 @@
+local lon = require("LinaTsukusu-CCOC/lon")
+
+local Position = {}
+
+function Position.new(x, y, z, face)
+    local this = {
+        x = tonumber(x) or 0,
+        y = tonumber(y) or 0,
+        z = tonumber(z) or 0,
+        face = face or 0,
+    }
+    setmetatable(this, {__index = Position})
+    return this
+end
+
+function Position:reset()
+    for k, v in pairs(self) do
+        self[k] = 0
+    end
+end
+
+function Position:save(filename)
+    lon.save(filename, self)
+end
+
+function Position.load(filename)
+    local data = lon.load(filename) or {}
+    return Position.new(data.x, data.y, data.z, data.face)
+end
+
+return Position
